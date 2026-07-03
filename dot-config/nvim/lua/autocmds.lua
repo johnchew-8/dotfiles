de610a777639a2e72335690ff4f1a42a5fb9e94e
JohnC <108507081+Viper-31@users.noreply.github.com>
2026-07-03 23:17:26 +0800

    fix(nvim): .jsonc commentstring

--- dot-config/nvim/lua/autocmds.lua
+++ dot-config/nvim/lua/autocmds.lua
@@ -1,1 +1,10 @@
 require "nvchad.autocmds"
+
+-- .jsonc comment autocmd
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "jsonc",
+  callback = function()
+    vim.bo.commentstring = "// %s"
+  end,
+  }
+)
