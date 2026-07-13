71e25db471add44860df7d4db31fb5c32d5bd00a
JohnC <108507081+Viper-31@users.noreply.github.com>
2026-07-13 21:34:02 +0800

    fix(jqfz): fzf evaluates expression into quoted string

--- Configs/fish/.config/fish/functions/jqfz.fish
+++ Configs/fish/.config/fish/functions/jqfz.fish
@@ -6,16 +6,10 @@ function jqfz --description 'Interactive jq JSON explorer with fzf' --argument-n
 
     set -f safe_path (string escape -- $file_path)
 
-    # Detect JSONC files -strip // , /**/ comment line before jq processing
-    set -f json_input "$file_path"
-    if string match --quiet '*.jsonc' "$file_path"
-        set json_input (sed 's|//.*||' "$safe_path" | psub)
-    end
-
     set -f selected_path (
       jq -r 'paths(scalars) as $p | [$p | join("."), ($p | @json)] | @tsv' "$file_path" | \
-      _fzf_wrapper --delimiter= '\t' --with-nth=1 \
-      --preview "jq -C 'getpath({2} | fromjson)' $safe_path" \
+      _fzf_wrapper --delimiter="\t" --with-nth=1 \
+      --preview "jq -C --argjson p {2} 'getpath(\$p)' $safe_path" \
       --prompt="jq> " \
       --header="Ctrl-C to abort, Enter to select"
     )
@@ -26,5 +20,5 @@ function jqfz --description 'Interactive jq JSON explorer with fzf' --argument-n
     end
 
     set -f json_path (string split --field 2 \t "$selected_path")
-    jq -r "getpath($json_path | fromjson)" $json_input
+    jq -r --argjson p "$json_path" 'getpath($p)' "$file_path"
 end
