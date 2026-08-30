410b39f81b4a400f39884df1a15acaf14a333a16
John C <108507081+Viper-31@users.noreply.github.com>
2026-08-30 18:19:04 +0800

    fix(nvim): Add :MasonEnsure to install tools on fresh machine

---
+++ Configs/nvim/.config/nvim/lua/configs/mason-tools.lua
@@ -0,0 +1,34 @@
+-- Mason registry names, not lspconfig/conform names
+local tools = {
+  -- LSP servers
+  "sqls",
+  "pyright",
+  "svelte-language-server",
+  "yaml-language-server",
+  "tailwindcss-language-server",
+  "typescript-language-server",
+  "rust-analyzer",
+  "lua-language-server",
+  "clangd",
+  "css-lsp",
+  "fish-lsp",
+  "gopls",
+  "html-lsp",
+  "json-lsp",
+  "marksman",
+  "taplo",
+  -- Linters
+  "eslint_d",
+  "typos",
+  "ruff",
+  "sqlfluff",
+  -- Formatters
+  "prettier",
+  "stylua",
+  "kdlfmt",
+  -- DAP
+  "delve",
+  "debugpy",
+}
+
+return tools
